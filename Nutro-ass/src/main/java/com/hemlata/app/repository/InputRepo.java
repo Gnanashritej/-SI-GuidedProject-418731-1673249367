b41de4488f1c1a510382f837ad2c166d694e8ffc
Gnanashritej <114292832+Gnanashritej@users.noreply.github.com>
2023-01-16 22:48:20 +0530

    Delete Nutro-ass directory

--- Nutro-ass/src/main/java/com/hemlata/app/repository/InputRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hemlata.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hemlata.app.model.Input;
-
-public interface InputRepo extends CrudRepository<Input,String>{
-
-}
