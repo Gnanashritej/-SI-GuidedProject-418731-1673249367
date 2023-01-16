ac64c01d4b6fa2ae3fb64c8170dcfdf2add931ea
Gnanashritej <114292832+Gnanashritej@users.noreply.github.com>
2023-01-16 22:46:59 +0530

    Delete MongoDBCrudOpProducts directory

--- MongoDBCrudOpProducts/src/main/java/com/hemlata/demo/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hemlata.demo.repositories;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hemlata.demo.domain.Product;
-
-public interface ProductRepository extends CrudRepository<Product, String> {
-}
